74d8283f3dd68fe62319322b47d2ee1bb4c307ac
dimitarKolev99 <mitko9966@gmail.com>
2025-02-26 19:51:28 +0100

    use kafka streams to join streams from payment service and inventory service

--- order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
@@ -3,6 +3,7 @@ package com.techie.microservices.order.service;
 import com.techie.microservices.order.client.InventoryClient;
 import com.techie.microservices.order.dto.OrderRequest;
 import com.techie.microservices.order.event.OrderPlacedEvent;
+import com.techie.microservices.order.event.OrderReceivedEvent;
 import com.techie.microservices.order.model.Order;
 import com.techie.microservices.order.repository.OrderRepository;
 import com.techie.microservices.order.util.Constants;
@@ -40,10 +41,34 @@ public class OrderService {
             orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
             orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
             log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
-            kafkaTemplate.send(Constants.ORDER_PLACED_TOPIC_NAME, orderPlacedEvent);
+            kafkaTemplate.send(Constants.ORDER_PLACED_NOTIFICATION_TOPIC_NAME, orderPlacedEvent);
             log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
         } else {
             throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
         }
+    }
+
+    public OrderReceivedEvent confirm(OrderReceivedEvent orderPayment, OrderReceivedEvent inventory) {
+        OrderReceivedEvent o = new OrderReceivedEvent();
+        o.setId(orderPayment.getId());
+        o.setProductId(orderPayment.getProductId());
+        o.setQuantity(orderPayment.getQuantity());
+        o.setPrice(orderPayment.getPrice());
+
+        if (orderPayment.getStatus().equals(Constants.ACCEPT_STATUS) &&
+                inventory.getStatus().equals(Constants.ACCEPT_STATUS)) {
+            o.setStatus(Constants.ACCEPT_STATUS);
+        } else if (orderPayment.getStatus().equals(Constants.REJECT_STATUS) &&
+                        inventory.getStatus().equals(Constants.REJECT_STATUS)) {
+            o.setStatus(Constants.REJECT_STATUS);
+        } else if (orderPayment.getStatus().equals(Constants.REJECT_STATUS) ||
+                        inventory.getStatus().equals(Constants.REJECT_STATUS)) {
+            String source = orderPayment.getStatus().equals(Constants.REJECT_STATUS)
+                    ? Constants.PAYMENT_STATUS : Constants.STOCK_STATUS;
+            o.setStatus(Constants.ROLLBACK_STATUS);
+            o.setSource(source);
+        }
+
+        return o;
     }
 }
